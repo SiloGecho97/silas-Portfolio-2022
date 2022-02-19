cb386003eb768f47a9cc5cfdf87d297f8d742620
Silas Getachew <silas.getachew@pairing.dev>
2022-02-19 14:11:48 +0300

    fix: resume link and font size

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Silas-resume.pdf";
+  "https://raw.githubusercontent.com/silogecho97/portfolio/master/src/Assets/Silas-resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
